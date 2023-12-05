ef0de0279966ab357af8178031d807b2fd124546
ts_saito <saito@lyzon.co.jp>
2023-12-05 15:26:46 +0900

    fix:MVCページ修正

--- mvc/Controllers/HomeController.cs
+++ mvc/Controllers/HomeController.cs
@@ -6,15 +6,10 @@ namespace SampleMVCApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
 
     public IActionResult Index()
     {
+        ViewBag.Hello = "Hello World.";
         return View();
     }
 
@@ -23,9 +18,4 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
