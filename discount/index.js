3b592715e4bfe7dc2c6fc57836b9a81712e835c7
ts_saito <saito@lyzon.co.jp>
2023-12-05 15:08:25 +0900

    feat:割引率計算の課題を追加(じゃんけんよりも簡単なやつ)

---
+++ discount/index.js
@@ -0,0 +1,30 @@
+//index.htmlの各要素を取得する
+const btnEl = document.getElementById("calculate"); //ボタンを取得
+const priceInput = document.getElementById("price"); //商品の金額(税抜)を取得
+const discountInput = document.getElementById("discount"); //割引率を取得
+const totalSpan = document.getElementById("total"); //金額(税抜)を取得
+const totaltaxSpan = document.getElementById("totaltax"); //金額(10％税込)を取得
+
+//ボタンを押した時に計算する関数
+function calculateTotal() {
+  //入力された金額、割引率を取得する
+  const priceValue = priceInput.value;
+  const discountValue = discountInput.value;
+
+  //計算した金額を代入する変数
+  let totalValue;
+  let totaltaxValue;
+
+  //割引した金額(税抜)を計算してtotalValueに代入する
+  totalValue = priceValue * (1 - discountValue / 100);
+  
+  //割引した後の金額の税込金額を計算してtotaltaxValueに代入する
+  totaltaxValue = (priceValue * (1 - discountValue / 100)) * 1.1;
+  
+  //計算した金額を表示する
+  totalSpan.innerText = Math.round(totalValue) + "円";
+  totaltaxSpan.innerText = Math.round(totaltaxValue) + "円";
+}
+
+//ボタンにイベントを設定する
+btnEl.addEventListener("click", calculateTotal);
